fda110e5930c6a16b8a342808df9d347cc07908f
jackwilc <jack@jacks-macbook-pro.local>
2015-02-22 15:52:43 +0000

    Revert d1df4a5..cf1a4a2
    This rolls back to commit d1df4a53bf0954899a440509233364d38a89283a.

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,3 +1,4 @@
+//$.backstretch("/images/background1.jpg");
 var socket = io.connect('http://192.168.0.3'); // change to appropiate for network access
 var $pad = $(".pad")
                             .xy({
@@ -11,7 +12,43 @@ var $pad = $(".pad")
                                         socket.emit('notedown',{message: value[0], message1: value[1]});
                                     }
                                 })
-                            .css({'border':'5px solid #fff'});
+                            .css({'border':'4px dashed #fff'});
+
+var dial = $(".dial")
+                    .dial({
+                            min: 0
+                            , max:127
+                            , fgColor:"#fff"
+                            , bgColor:"#333333"
+                            , cursor: 50
+                            , change : function (value) {
+                                        //console.log("change : ", value);
+                                        socket.emit('dialchange',{message: value});
+                                    }
+                        })
+                    .css({display:'inline'});
+
+
+$(".bars").bars({
+                            fgColor:"#fff"
+                            , bgColor:"#EEEEEE"
+                            , change : function (value) {
+                                        console.log("change : ", value);
+
+                                        if(value[0] !== undefined){
+                                        socket.emit('bar1change',{message: value[0]});
+                                        }
+                                        if(value[1] !== undefined){
+                                        socket.emit('bar2change',{message: value[1]});
+                                        }
+                                        if(value[2] !== undefined){
+                                        socket.emit('bar3change',{message: value[2]});
+                                        }
+                                        if(value[3] !== undefined){
+                                        socket.emit('bar4change',{message: value[3]});
+                                        }
+                                    }
+                        });
 
                              var pulse = new Pulse();
                              pulse.connect('http://192.168.0.3');
@@ -19,8 +56,20 @@ var $pad = $(".pad")
                                 // document.getElementById('record').style.transform = 'rotate(' + (pulse.beat() % 1) * 360 + 'deg)';
                                 // var r = 'rotate(' + (pulse.beat() % 8) * 45 + 'deg)';
                                 // $('#deck div').css('transform', r);
-                                $('#overlay').css('opacity', pulse.pulse() * 0.8)
+                                $('#overlay').css('opacity', pulse.pulse() * 0.1)
+                                //$("#redbar").css('width', pulse.pulse() * 100 + '%');
+                                $("p#lefttext").text('Network Latency: ' + Math.round(pulse.netLatency) + 'ms');
+                                $("p#bpm").text('BPM: ' + Math.round(pulse.bpm));
                                 setTimeout(show, 10);
                             }
                              show();
 
+
+document.onmousedown=disableclick;
+function disableclick(event)
+{
+  if(event.button==2)
+   {
+     return false;    
+   }
+}
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,11 @@
 exports.index = function(req, res){
   res.render('index');
 };
+
+exports.dial = function(req, res){
+  res.render('dial');
+};
+
+exports.bars = function(req, res){
+  res.render('bars');
+};
